8cf8b84aaac8a998f80b4d606877996db8635f81
Randy Schmidt <randy@umlatte.com>
2008-09-03 20:45:14 -0400

    Fix loading of aws-s3

--- init.rb
+++ init.rb
@@ -1,2 +1,1 @@
-$: << File.expand_path(File.dirname(__FILE__) + "/vendor/aws-s3-0.5.1/lib")
-require 'lib/backup_fu'
+require 'backup_fu'
--- lib/backup_fu.rb
+++ lib/backup_fu.rb
@@ -1,5 +1,6 @@
 require 'yaml'
 require 'active_support'
+$: << File.expand_path(File.dirname(__FILE__) + "/../vendor/aws-s3-0.5.1/lib/")
 require 'aws/s3'
 
 class BackupFuConfigError < StandardError; end
